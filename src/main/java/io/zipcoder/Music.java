18d82c516316d42c524e45dec6f780b8da09a983
April Howard <ahoward@zipcodes-MBP-5.fios-router.home>
2020-02-23 17:15:19 -0500

    add method

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -1,14 +1,46 @@
 package io.zipcoder;
 
+import java.util.Arrays;
+
 public class Music {
 
     private String[] playList;
 
-    public Music(String[] playList){
+    public Music(String[] playList) {
         this.playList = playList;
     }
 
-    public Integer selection(Integer startIndex, String selection){
-        return null;
+    public Integer selection(Integer startIndex, String selection) {
+
+
+        int a = Arrays.asList(playList).indexOf(startIndex)+1;
+
+        int b = Arrays.asList(playList).indexOf(selection)-1;
+
+
+            return  b-a;
+        }
+
     }
-}
+
+
+
+
+
+        /*
+        logic:  a. find starting index of song playing.
+                b. find index of desired song
+                c. return difference of song playing and desired song
+
+            if index of b > index of a {
+            return b -a;
+            }
+            else {
+            if index b < index a {
+            return a - b
+            }
+
+         */
+
+
+
